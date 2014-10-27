fe6192bcf7bcd0983f60e650816d3b9eeec2c87e
Bradley <bradley.m.walker@gmail.com>
2014-10-27 17:28:16 -0500

    Added custom New Post View Controller and storyboard elements for adding photo from camera roll

---
+++ InstaPro/NewPostViewController.m
@@ -0,0 +1,22 @@
+//
+//  NewPostViewController.m
+//  InstaPro
+//
+//  Created by Bradley Walker on 10/27/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import "NewPostViewController.h"
+
+@interface NewPostViewController ()
+
+@end
+
+@implementation NewPostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
