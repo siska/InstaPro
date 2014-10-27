ae984dbe6eb6f41563315be2aa9a3ab3ae2e2efa
Wade Sellers <Waderoni@gmail.com>
2014-10-27 17:44:05 -0500

    labels and text fields set up for segmented controller as well as login button setup for parse entry/retrieval

--- InstaPro/RootViewController.m
+++ InstaPro/RootViewController.m
@@ -8,10 +8,13 @@
 
 #import "RootViewController.h"
 
+
 @interface RootViewController ()
 @property (weak, nonatomic) IBOutlet UISegmentedControl *loginSignUpToggle;
 @property (weak, nonatomic) IBOutlet UITextField *username;
 @property (weak, nonatomic) IBOutlet UITextField *password;
+@property (weak, nonatomic) IBOutlet UITextField *verifyPassword;
+@property (weak, nonatomic) IBOutlet UILabel *instructionLabel;
 
 @end
 
@@ -19,13 +22,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.verifyPassword.hidden = YES;  //Default toggled to Sign-In so this is hidden
+}
 
-
+- (IBAction)loginSignUpSwitched:(id)sender
+{
+    if (self.loginSignUpToggle.selectedSegmentIndex == 0)
+    {
+        self.verifyPassword.hidden = YES;
+        self.instructionLabel.text = @"Please Login Below";
+    }
+    else
+    {
+        self.verifyPassword.hidden = NO;
+        self.instructionLabel.text = @"Please Sign-Up Below";
+    }
 }
 
 - (IBAction)onLoginButtonPressed:(id)sender
 {
-    
+    if (self.loginSignUpToggle.selectedSegmentIndex == 0)
+    {
+        //pull parse data for user
+    }
+    else //It will be on the Sign-up Screen so we can move right into those instructions
+    {
+        if (![self.password.text isEqualToString:self.verifyPassword.text])
+        {
+            self.verifyPassword.text = @"Passwords do not match. Try Again";
+        }
+        else
+        {
+            //enter new user into parse as an object
+        }
+    }
+
 }
 
 
