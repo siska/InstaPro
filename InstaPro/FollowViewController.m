087da9f617b838d2b5d770f6e4bb81e503def5b9
Bradley <bradley.m.walker@gmail.com>
2014-10-29 16:32:20 -0500

    removed comments from FollowViewController.m

--- InstaPro/FollowViewController.m
+++ InstaPro/FollowViewController.m
@@ -12,7 +12,7 @@
 
 @interface FollowViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
-@property NSArray *followUsers;
+@property NSArray *allUsers;
 @end
 
 @implementation FollowViewController
@@ -25,30 +25,24 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.followUsers.count;
+    return self.allUsers.count;
 }
 
 -(FollowerTableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     FollowerTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FollowCell"];
-    PFUser *followUser = [self.followUsers objectAtIndex:indexPath.row];
+    PFUser *followUser = [self.allUsers objectAtIndex:indexPath.row];
     cell.textLabel.font = [UIFont systemFontOfSize:17];
     cell.textLabel.text = followUser.username;
-    cell.followIcon.image = [UIImage imageNamed:@"followUser"];
 
-//    PFQuery *followQuery = [PFUser query];
-//    [followQuery whereKey:@"owner" equalTo:person];
-//    [followQuery countObjectsInBackgroundWithBlock:^(int number, NSError *error) {
-//        cell.detailTextLabel.text = @(number).stringValue;
-//    }];
+
+    cell.followIcon.image = [UIImage imageNamed:@"followUser"];
 
     return cell;
 }
 
 - (void) refreshDisplay
 {
-//    if (self.viewingFollowers)
-//    {
             PFQuery *followingQuery = [PFQuery queryWithClassName:[PFUser parseClassName]];
     [followingQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
      {
@@ -58,27 +52,10 @@
          }
          else
          {
-             self.followUsers = objects;
+             self.allUsers = objects;
              [self.tableView reloadData];
          }
      }];
-//    }
-//    else
-//    {
-//        PFQuery *followedQuery = [PFQuery queryWithClassName:@"UserFollows"];
-//        [followedQuery whereKey:@"followee" equalTo:[PFUser currentUser]];
-//        [followedQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-//            if (error)
-//            {
-//                NSLog(@"%@", error);
-//            }
-//            else
-//            {
-//                self.followUsers = objects;
-//                [self.tableView reloadData];
-//            }
-//        }];
-//    }
 }
 
 -(void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath
