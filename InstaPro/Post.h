6ad9058077d5cb4135d2052cea9032db584a572b
Wade Sellers <Waderoni@gmail.com>
2014-10-28 20:40:36 -0500

    added a note in post.h

--- InstaPro/Post.h
+++ InstaPro/Post.h
@@ -16,3 +16,6 @@
 @property User *user;
 
 @end
+
+
+//adam said that when we post photos we can also pass the current user into the parse table so it has that attachment too.  Not sure if we want or need it, but he just said it.
