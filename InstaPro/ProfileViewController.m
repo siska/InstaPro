d5cb769e9050c604585c78b3707cfcbde5399dc3
Ryan Siska <rsiska1@gmail.com>
2014-10-29 16:29:36 -0500

    Pulled master - updating Profile to show larger images and correct follwer and post counts

--- InstaPro/ProfileViewController.m
+++ InstaPro/ProfileViewController.m
@@ -12,7 +12,7 @@
 
 @interface ProfileViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 @property NSArray *user;
-@property NSArray *photo;
+@property NSArray *photos;
 @property (strong, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (strong, nonatomic) IBOutlet UILabel *labelPosts;
 //@property (strong, nonatomic) IBOutlet UILabel *labelFollowers;
@@ -29,45 +29,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.currentImages = [NSMutableArray new];
-    [self refreshDisplayWithUserInfo];
+    [self setUserProfileInformation];
     [self refreshDisplayWithUserPhotos];
-}
-
--(void)refreshDisplayWithUserInfo
-{
-    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (error) {
-            NSLog(@"Error: %@", error.userInfo);
-            self.user = [NSArray array]; //5 - want to set to null so app doesn't crash, app expects some array back after this method
-        }
-        else {
-            self.user = objects;
-            [self setUserProfileInformation];
-        }
-    }];
 }
 
 -(void)setUserProfileInformation
 {
-    PFObject *person = self.user.firstObject; //will have to change to say I want a specific user - the main user
-    //self.profileImageView = person[@"profilePhoto"]; - will likely have to actually convert this from data
-    self.labelUsername.text = person[@"username"];
-    NSLog(@"Username: %@", person[@"username"]);
-    // no array yet - self.labelPosts.text = person[@"postArray"].count;
-    //will use this space to set
+    self.labelUsername.text = [PFUser currentUser].username;
 }
 
 -(void)refreshDisplayWithUserPhotos
 {
-    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+    PFQuery *queryForPosts = [PFQuery queryWithClassName:@"Post"];
+    [queryForPosts whereKey:@"user" equalTo:[PFUser currentUser]];
+    [queryForPosts findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (error) {
             NSLog(@"Error: %@", error.userInfo);
-            self.photo = [NSArray array]; //5 - want to set to null so app doesn't crash, app expects some array back after this method
+            self.photos = [NSArray array];
         }
         else {
-            self.photo = objects;
+            self.photos = objects;
             [self setPhotosForUser];
         }
     }];
@@ -75,20 +56,20 @@
 
 -(void)setPhotosForUser
 {
-    PFObject *photo = self.photo.firstObject;
-    PFFile *file = photo[@"photoData"];
-
-
-    //PFFile *file = [PFObject objectWithClassName:@"photoData"];
-    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
-        if (!error) {
-            UIImage *image = [UIImage imageWithData:data];
-            [self.currentImages addObject:image];
+    for (PFObject *photo in self.photos)
+    {
+        PFFile *file = photo[@"photoData"];
+        [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            if (!error) {
+                UIImage *image = [UIImage imageWithData:data];
+                [self.currentImages addObject:image];
+                //self.usersPhotosImageView.image = image;
+                // image can now be set on a UIImageView
+            }
             [self.imageCollectionView reloadData];
-            //self.usersPhotosImageView.image = image;
-            // image can now be set on a UIImageView
-        }
-    }];
+        }];
+
+    }
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
